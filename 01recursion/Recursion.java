e69a9b58827aeea0f0ad0966e1750d789e27f4f5
Mindy Ruan <mindyruan@users.noreply.github.com>
2015-02-07 22:14:28 -0500

    fibonacci

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -3,13 +3,21 @@ public class Recursion{
     public static void main(String[]args){
 	Recursion r = new Recursion();
 	
-	/*
+	/*FACTORIAL TESTS
 	System.out.println(r.fact(0));
 	System.out.println(r.fact(1));
 	System.out.println(r.fact(4));
-	System.out.println(r.fact(5));
 	System.out.println(r.fact(11));
 	*/
+
+	/*FIBONACCI TESTS
+	System.out.println(r.fib(0));
+	System.out.println(r.fib(1));
+	System.out.println(r.fib(2));
+	System.out.println(r.fib(4));
+	System.out.println(r.fib(11));
+	*/
+	
 	
     }
 
@@ -25,6 +33,16 @@ public class Recursion{
 	    return n;
 	}
 	return n * fact(n-1);
+    }
+
+    public int fib(int n){
+	if (n < 0){
+	    throw new IllegalArgumentException();
+	}
+	if ((n == 1) || (n == 0)){
+	    return n;
+	}
+	return fib(n-1) + fib(n-2);
     }
 
 }

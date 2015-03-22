6ce17db0442c9b558f8b1b87ff7eaa505d81886f
Mindy Ruan <mindyruan@users.noreply.github.com>
2015-03-22 13:09:39 -0400

    generics and added empty constructor

--- 07linkedlist/LNode.java
+++ 07linkedlist/LNode.java
@@ -1,25 +1,29 @@
-public class LNode{
+public class LNode<T>{
 
-    private int value;
-    private LNode next;
+    private T value;
+    private LNode<T> next;
 
-    public LNode(int val){
+    public LNode(){
+    
+    }
+
+    public LNode(T val){
 	value = val;
     }
 
-    public void setValue(int val){
+    public void setValue(T val){
 	value = val;
     }
 
-    public int getValue(){
+    public T getValue(){
 	return value;
     }
 
-    public void setNext(LNode n){
+    public void setNext(LNode<T> n){
 	next = n;
     }
 
-    public LNode getNext(){
+    public LNode<T> getNext(){
 	return next;
     }
 

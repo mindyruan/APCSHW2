833a9b4f4b98e2fdaa38dc739543ebf45bea2121
Mindy Ruan <mindyruan@users.noreply.github.com>
2015-03-22 13:33:40 -0400

    ¯\_(ツ)_/¯

---
+++ 09phatstacks/MyStack.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class MyStack<T>{
+
+    private MyLinkedList<T> stack;
+
+    public MyStack(){
+	stack = new MyLinkedList<T>();
+    }
+
+    public String name(){
+	return "ruan.mindy";
+    }
+
+    public void push(T val){
+
+    }
+
+    public void pop(){
+
+    }
+
+    public void peek(){
+
+    }
+
+    public void empty(){
+
+    }
+
+    public static void main(String[]arg){
+	System.out.println("ech");
+    }
+
+}

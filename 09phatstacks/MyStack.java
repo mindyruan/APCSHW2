85b2ccaa70a218dc166e8b18c4ef4ab569726053
Mindy Ruan <mindyruan@users.noreply.github.com>
2015-03-22 13:58:39 -0400

    ¯\_(ツ)_/¯

--- 09phatstacks/MyStack.java
+++ 09phatstacks/MyStack.java
@@ -12,24 +12,50 @@ public class MyStack<T>{
 	return "ruan.mindy";
     }
 
-    public void push(T val){
-
+    public void push(T val){ //add to end
+        stack.add(val);
     }
 
-    public void pop(){
-
+    public T pop(){ //remove last element
+	if (empty()){
+	    throw new EmptyStackException();
+	}
+	return stack.remove(stack.size()-1);
     }
 
-    public void peek(){
-
+    public T peek(){ //get last
+	if (empty()){
+	    throw new EmptyStackException();
+	}
+	return stack.get(stack.size()-1);
     }
 
-    public void empty(){
+    public boolean empty(){
+	return stack.size() == 0;
+    }
 
+    public String toString(){
+	return stack.toString();
     }
 
     public static void main(String[]arg){
-	System.out.println("ech");
+	MyStack<String> s = new MyStack<String>();
+
+	s.push("a");
+	s.push("fish");
+	s.push("bruh");
+	s.push("ech");
+	System.out.println(s);
+
+	System.out.println(s.peek());
+	s.pop();
+	System.out.println(s);
+	System.out.println(s.peek());
+	s.pop();
+	System.out.println(s);
+	System.out.println(s.peek());
+
+
     }
 
 }

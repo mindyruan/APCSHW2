852dc674a8ccb75955d45e64602b48029c0918d0
Mindy Ruan <mindyruan.763002@gmail.com>
2015-04-23 12:59:08 -0400

    base stuff

---
+++ 12TreeLab/TreeNode.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.*;
+
+public class TreeNode<E>{
+
+    private E data;
+    private TreeNode<E> left;
+    private TreeNode<E> right;
+
+    public TreeNode(){
+	//constructor
+    }
+
+    public TreeNode<T>(){
+
+    }
+
+    public E getData(){
+	return data;
+    }
+
+    public TreeNode<E> getLeft(){
+	return left;
+    }
+
+    public TreeNode<E> getRight(){
+	return right;
+    }
+
+    public void setData(E val){
+	data = val;
+    }
+
+    public void setLeft(TreeNode<E> l){
+	left = l;
+    }
+
+    public void setRight(TreeNode<E> r){
+	right = r;
+    }
+
+}
